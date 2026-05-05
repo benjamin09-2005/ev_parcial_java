c52159e291669e11900202cdeca0cfe12fa86a94
Benjita <ben.barahona@duocuc.cl>
2026-05-04 23:19:04 -0400

    codigo de creacion de catgoria y producto

--- src/main/java/com/evjava/demo/model/Producto.java
+++ src/main/java/com/evjava/demo/model/Producto.java
@@ -1,4 +1,34 @@
 package com.evjava.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "producto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre del producto es obligatorio")
+    private String nombre;
+
+    @Min(value = 0, message = "El precio no puede ser negativo")
+    private Double precio;
+
+    private String imagen;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id")
+    @JsonIgnore
+    private Categoria categoria;
 }
