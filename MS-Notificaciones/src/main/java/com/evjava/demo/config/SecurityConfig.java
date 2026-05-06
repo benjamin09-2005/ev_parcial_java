8b99e4f2fbd77b2bcc6d39d840db1efcd3390c5f
Renato416 <re.rojasc@duocuc.cl>
2026-05-05 21:54:34 -0400

    Configuracion h2 x cada ms

--- MS-Notificaciones/src/main/java/com/evjava/demo/config/SecurityConfig.java
+++ MS-Notificaciones/src/main/java/com/evjava/demo/config/SecurityConfig.java
@@ -13,10 +13,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                // Necesario para H2
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+
+                // Permisos
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 }
